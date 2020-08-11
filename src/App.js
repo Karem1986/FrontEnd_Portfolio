dc41db00f22f49ee1485da1aac48e8d26373d807
Karem1986 <karem.ortiz@outlook.com>
2020-08-11 16:15:35 +0200

    created navigation-user story 1.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import logo from "./logo.jpg"
+import NavBar from "./components/Navigation"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <h1><img src={logo} alt="Polshop" style={{ width: 300, display: 'inline' }}
+      ></img>POLshop.com: where your dreams come true!</h1>
 
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import React from 'react'
+export default function NavBar() {
+    return (
+        <div>
+            <NavLink to="/">Home </NavLink>
+            <NavLink to="/signup">Signup </NavLink>
+            <NavLink to="/login">Login </NavLink>
+            <NavLink to="/contact">Contact </NavLink>
+
+
+        </div>
+    )
+
+
+
+}
