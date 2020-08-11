d254c0892333f091b3424da717e587f78e85da43
Karem1986 <karem.ortiz@outlook.com>
2020-08-11 17:48:41 +0200

    set up apollo client, apollo provider-success

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import logo from "./logo.jpg"
 import NavBar from "./components/Navigation"
 import './App.css';
+import { gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+
+})
+//testing our Apollo setup:
+client
+  .query({
+    query: gql`
+      query {
+       allCategories {
+         type
+       }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <h1><img src={logo} alt="Polshop" style={{ width: 300, display: 'inline' }}
-      ></img>POLshop.com: where your dreams come true!</h1>
-
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <NavBar />
+        <h1><img src={logo} alt="Polshop" style={{ width: 300, display: 'inline' }}
+        ></img>POLshop.com: where your dreams come true!</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default function Homepage() {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Homepage
