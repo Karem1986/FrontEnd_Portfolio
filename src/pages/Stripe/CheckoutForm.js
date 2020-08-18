b126972d3dacbdd2a11e0dd0ec0adcf140f2c8dc
Karem1986 <karem.ortiz@outlook.com>
2020-08-18 18:34:03 +0200

    stripe integration: success!

--- src/pages/Stripe/CheckoutForm.js
+++ src/pages/Stripe/CheckoutForm.js
@@ -1,55 +1,123 @@
 import React, { useState } from "react"
-import { Elements } from "@stripe/react-stripe-js"
+import { useQuery, gql, useMutation } from "@apollo/client"
+
 import { loadStripe } from "@stripe/stripe-js"
 
+import {
+    useStripe,
+    Elements,
+    CardElement,
+    useElements,
+} from "@stripe/react-stripe-js"
+
+import "./CheckoutForm.css"
+const firstPaymentCall = gql`
+    mutation createpayment($amount: Int) {
+        createpayment(amount: $amount) {
+            client_secret
+        }
+    }
+`
+
 const stripePromise = loadStripe(
-    "pk_live_51HHCvXLtQj1lsGQL7Dx4UVQ1dB2IRxGly5MzGlKoQokxq44A02TpUxIdMbKdGvsTNDJFulILZpwBXSa1rR9NWvBE00ZcsVRanr"
+    "pk_test_51HHCvXLtQj1lsGQLqH9LEEQa8w7UoN4yFXvJHaGfBOFbWbUoF3p4Ff9yI0dvHp7SF9rhLdXArbP1TougSt13csHU00VAg5ufqa"
 )
-export default function CheckoutForm() {
+function CheckoutForm() {
+    const stripe = useStripe()
+    const elements = useElements()
     const [name, setName] = useState("")
     const [address, setAddress] = useState("")
-
-    function onSubmit(event) {
+    //1.mutation here for payment: get back my client secret
+    const [payment] = useMutation(firstPaymentCall)
+    console.log("firstPaymentCall", payment)
+    async function onSubmit(event) {
         event.preventDefault()
         console.log(`
-        Name: ${name} 
-        Email: ${address}
-      
-   
-    `)
+            Name: ${name} 
+            Email: ${address}
+        `)
+
+        const res = await payment({
+            variables: {
+                amount: 51,
+            },
+        })
+        console.log("res", res)
+        const result = await stripe.confirmCardPayment(
+            res.data.createpayment.client_secret,
+            {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                    billing_details: {
+                        name: name,
+                        address: address,
+                    },
+                },
+            }
+        )
+        console.log("result", result)
     }
 
     function onChange(event, set_Function) {
         set_Function(event.target.value)
     }
+    const CARD_ELEMENT_OPTIONS = {
+        style: {
+            base: {
+                color: "#32325d",
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSmoothing: "antialiased",
+                fontSize: "16px",
+                "::placeholder": {
+                    color: "#aab7c4",
+                },
+            },
+            invalid: {
+                color: "#fa755a",
+                iconColor: "#fa755a",
+            },
+        },
+    }
 
     return (
-        <Elements stripe={stripePromise}>
-            <div className="form">
-                <form
-                    onSubmit={onSubmit}
-                    style={{
-                        color: "#32325d",
-                        fontFamily: "Helvetica, sans-serif",
-                        fontSize: "20px",
-                        backgroundColor: "turquoise",
-                        margin: "50px",
-                    }}
-                >
-                    <h2>Please complete with your information below:</h2>
-                    <input
-                        className="input"
-                        onChange={(event) => onChange(event, setName)}
-                    />
-                    <p>Name</p>
-                    <input
-                        className="input"
-                        onChange={(event) => onChange(event, setAddress)}
-                    />
-                    <p>Address</p>
-                    <button className="button">Submit</button>
-                </form>
-            </div>
-        </Elements>
+        <div className="form">
+            <form
+                onSubmit={onSubmit}
+                style={{
+                    color: "white",
+                    fontFamily: "Helvetica, sans-serif",
+                    fontSize: "20px",
+                    backgroundColor: "black",
+                    margin: "100px",
+                    padding: "50px",
+                }}
+            >
+                <h2 style={{ color: "white" }}>
+                    Please complete your payment information below:
+                </h2>
+                <input
+                    className="input"
+                    onChange={(event) => onChange(event, setName)}
+                />
+                <p>Name</p>
+                <input
+                    className="input"
+                    onChange={(event) => onChange(event, setAddress)}
+                />
+                <p>Address</p>
+
+                <CardElement options={CARD_ELEMENT_OPTIONS} />
+
+                <button className="button" style={{ display: "flex" }}>
+                    Submit
+                </button>
+            </form>
+        </div>
     )
 }
+
+export default () => (
+    <Elements stripe={stripePromise}>
+        <CheckoutForm />
+    </Elements>
+)
